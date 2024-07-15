3ce10227fe7698552ba0c234bd209765173fbef2
Arhamsoft <wajiha.naveed@arhamsoft.org>
2024-07-15 16:02:03 +0500

    model training

--- app.py
+++ app.py
@@ -3,19 +3,26 @@ from src.MLProject.exception import CustomException
 from src.MLProject.components.data_ingestion import DataIngestion
 from src.MLProject.components.data_ingestion import DataIngestionConfig
 from src.MLProject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.MLProject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import sys
  
 if __name__ == "__main__":
     logging.info("The execution has been started.")
 
     try:
+        # Data Ingestion
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
+        # Data Transformation
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        # Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
     except Exception as e:
         logging.info("Custom Exception")

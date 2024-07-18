08525ef423f6c40a156e75958aa17ff8b21df00c
Arhamsoft <wajiha.naveed@arhamsoft.org>
2024-07-18 12:21:01 +0500

    commit

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
-from src.MLProject.logger import logging
-from src.MLProject.exception import CustomException
-from src.MLProject.components.data_ingestion import DataIngestion
-from src.MLProject.components.data_ingestion import DataIngestionConfig
-from src.MLProject.components.data_transformation import DataTransformationConfig, DataTransformation
-from src.MLProject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import sys
- 
-if __name__ == "__main__":
-    logging.info("The execution has been started.")
+import os
+from flask import Flask, request, render_template
+import pandas as pd
+from src.MLProject.exception import CustomException
+from src.MLProject.utils import load_object
+from src.MLProject.pipelines.prediction_pipeline import PredictPipeline, CustomData
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict_datapoint():
     try:
-        # Data Ingestion
-        #data_ingestion_config = DataIngestionConfig()
-        data_ingestion = DataIngestion()
-        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
-
-        # Data Transformation
-        #data_transformation_config = DataTransformationConfig()
-        data_transformation = DataTransformation()
-        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
-
-        # Model Training
-        model_trainer = ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
-
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course', 'none'),  # Set default if None
+            reading_score=int(request.form.get('reading_score')),
+            writing_score=int(request.form.get('writing_score'))
+        )
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html', results=results)
+    
     except Exception as e:
-        logging.info("Custom Exception")
         raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    app.run(debug=True)
